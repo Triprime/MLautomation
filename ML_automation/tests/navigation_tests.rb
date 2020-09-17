3531f8e508234a169ad0da71e9d0c1a674321fd1
Matt Shelton <m.shell.town@gmail.com>
2020-09-17 12:18:37 -0600

    Add user_fixtures.yaml

--- ML_automation/tests/navigation_tests.rb
+++ ML_automation/tests/navigation_tests.rb
@@ -4,9 +4,9 @@ require 'test/unit'
 
 require_relative '../helpers/selenium_helper'
 require_relative '../helpers/navigation_helper'
-#require_relative '../helpers/test_helper'
-#require_relative '../helpers/test_data'
-#require_relative '../helpers/page_data'
+require_relative '../helpers/test_helper'
+require_relative '../helpers/test_data'
+require_relative '../helpers/page_data'
 
 class NavigationTests < Test::Unit::TestCase
 
