b347512f9b71708c10c3a78e01a097d6376cd2ee
Matt Shelton <m.shell.town@gmail.com>
2020-09-17 11:41:47 -0600

    Add short nav test

--- ML_automation/tests/navigation_tests.rb
+++ ML_automation/tests/navigation_tests.rb
@@ -20,6 +20,28 @@ class NavigationTests < Test::Unit::TestCase
 		@selenium.quit
 	end
 
-	
+	# tests sequential navigation for Main navigation menu
+	# pass the page title text as a param
+
+	def test_main_navigation
+		verify_page_url		("Your Dashboard")
+		verify_page_title	("Your Dashboard")
+
+		main_navigation_to	("Projects")
+		verify_page_url		("Projects")
+		verify_page_title	("Projects")
+
+		# main_navigation_to	("Project Templates")
+		# verify_page_url		("Project Templates")
+		# verify_page_title	("Project Templates")
+
+		# main_navigation_to	("Tasks")
+		# verify_page_url		("Tasks")
+		# verify_page_title	("Tasks")
+
+		# main_navigation_to	("General Settings")
+		# verify_page_url		("General Settings")
+		# verify_page_title	("General Settings")
+	end
 
 end
