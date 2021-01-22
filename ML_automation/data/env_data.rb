3734060f285e4e7c9b2d541f82069ec8adc91887
Matt Shelton <m.shell.town@gmail.com>
2021-01-22 13:25:49 -0700

    remove unnecessary comments in env_data.rb

--- ML_automation/data/env_data.rb
+++ ML_automation/data/env_data.rb
@@ -1,11 +1,7 @@
 class EnvironmentData
 
 	def self.get_base_url
-		# set environment variable with one of the following console commands just before test run:
-		# export environment=production
-		# export environment=mwho
-		#
-		# before running tests on an environment, an account and members must be manually created,
+		# before running tests on an environment, an account and its members must be manually created,
 		# then added to user_data.rb
 		{
 			"production" 	=> "https://app.mavenlink.com", 		# Currently, no automated QA account set up
@@ -13,18 +9,13 @@ class EnvironmentData
 			"railsnext"		=> "https://app.railsnext.mvn.link",
 			"webinf"		=> "https://app.webinf.mvn.link",		# Currently, no automated QA account set up
 			"quickbooks"	=> "https://app.quickbooks.mvn.link",	# Currently, no automated QA account set up
-			"snow"			=> "https://app.snowpeople.mvn.link",	# Currently, no automated QA account set up
+			"snow"			=> "https://app.snowpeople.mvn.link",	
 			"staging1"		=> "https://app.staging1.mavenlink.net",
-			"rogue"			=> "https://app.roguesquadron.mvn.link",
+			"rogue"			=> "https://app.roguesquadron.mvn.link",# Currently, no automated QA account set up
 			"msync"			=> "https://app.msync.mvn.link",
-			"rpm"			=> "https://app.rpm.mvn.link"
+			"rpm"			=> "https://app.rpm.mvn.link"			# Currently, no automated QA account set up
 		}[self.get_environment]
 
-
-		# below are placeholder base urls when not using the environment variable (above) for test runs.
-		# comment out the hash above and uncomment the appropriate base url below
-		# "https://app.mavenlink.com"
-		# "https://app.mwho.mvn.link"
 	end
 
 	def self.get_environment
